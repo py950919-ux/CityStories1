c537b39ff8afb69168ce8b296191a8e8c889d7a3
py950919-ux <py950919@gmail.com>
2026-05-12 16:03:31 +0530

    Remove Toaster component from App.tsx
    Removed Toaster component from the App component.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider } from "@/lib/auth";
 import { ProtectedRoute } from "@/components/protected-route";
@@ -81,7 +80,6 @@ function App() {
           <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
             <Router />
           </WouterRouter>
-          <Toaster />
         </TooltipProvider>
       </AuthProvider>
     </QueryClientProvider>
